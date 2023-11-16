13cf0b66447e7230c0861305faf5cb7800ce649f
Matt <matthewjosefcruz@gmail.com>
2023-11-16 18:34:16 +0800

    Add Function To Check Winner

--- script.js
+++ script.js
@@ -28,17 +28,33 @@ const gameboard = (() => {
             player1.turn = true;
 
         }
+        checkWinner();
         console.log(`board game array: ${gamePlaces}`);
     }));
 
-    /*const checkWinner = ((playerToken) => {
-        const tempTokenPlaces = [];
-        for(let i = 0; i < 9; i++) {
-            if (gamePlaces[i] === playerToken) tempTokenPlaces.push(i);
-        }
-        if (tempTokenPlaces.every(x => x.includes(0, 1)));
-        console.log(`Temporary Array: ${tempTokenPlaces}`);
-    });*/
+    const checkWinner = (() => {
+        const winningPlaces = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+        winningPlaces.forEach((set) => {
+            //Checks all three indexes, if same value then it checks if it is the symbol of one of the 2 players
+            if(gamePlaces[set[0]] === gamePlaces[set[1]] && gamePlaces[set[1]] === gamePlaces[set[2]] && gamePlaces[set[0]] === player1.token) {
+                console.log('player1 won');
+            } else if (gamePlaces[set[0]] === gamePlaces[set[1]] && gamePlaces[set[1]] === gamePlaces[set[2]] && gamePlaces[set[0]] === player2.token) {
+                console.log('player2 won');
+            }
+        });
+
+    });
+
+
 
     return {gamePlaces};
 })();
